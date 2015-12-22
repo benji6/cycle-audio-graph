cd898673f999c34c2679f9cd015ad666bf0c01ba
benji6 <benhall22@hotmail.co.uk>
2015-12-22 22:47:38 +0000

    remove semi-colons

--- index.es6.js
+++ index.es6.js
@@ -1,7 +1,7 @@
-import createVirtualAudioGraph from 'virtual-audio-graph';
+import createVirtualAudioGraph from 'virtual-audio-graph'
 
 export default ({audioContext = new AudioContext(),
                  output = audioContext.destination} = {}) => {
-  const virtualAudioGraph = createVirtualAudioGraph({audioContext, output});
-  return nodeParams$ => nodeParams$.subscribe(nodeParams => virtualAudioGraph.update(nodeParams));
-};
+  const virtualAudioGraph = createVirtualAudioGraph({audioContext, output})
+  return nodeParams$ => nodeParams$.subscribe(nodeParams => virtualAudioGraph.update(nodeParams))
+}
