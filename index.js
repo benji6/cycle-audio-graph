b5ba95a444c8c179466d02d4dc555bd88c1089fa
Ben Hall <benji2357@gmail.com>
2020-09-29 20:21:23 +0100

    refactor(makeAudioGraphDriver): name function and format source code

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-'use strict'
+"use strict";
 
-var createVirtualAudioGraph = require('virtual-audio-graph').default
+var createVirtualAudioGraph = require("virtual-audio-graph").default;
 
-module.exports = function (arg) {
-  var config = arg || {}
-  var audioContext = config.audioContext || new AudioContext
-  var output = config.output || audioContext.destination
+module.exports = function makeAudioGraphDriver(arg) {
+  var config = arg || {};
+  var audioContext = config.audioContext || new AudioContext();
+  var output = config.output || audioContext.destination;
 
   var virtualAudioGraph = createVirtualAudioGraph({
     audioContext: audioContext,
-    output: output
-  })
+    output: output,
+  });
 
   return function (nodeParams$) {
     return nodeParams$.addListener({
       next: function next(nodeParams) {
-        return virtualAudioGraph.update(nodeParams)
-      }
-    })
-  }
-}
+        return virtualAudioGraph.update(nodeParams);
+      },
+    });
+  };
+};
